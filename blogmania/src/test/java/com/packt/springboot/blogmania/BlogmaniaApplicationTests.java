c265789e41f4a8ac276b9e3066b92405ab66b4c9
Rui Pinheiro <rui.pinheiro@together.pt>
2020-02-01 12:54:48 +0000

    Book: Spring Boot 2 | Add base Blogmania project generated from Sprint Intializr (Maven, Java 2.2.4, no dependencies)

---
+++ blogmania/src/test/java/com/packt/springboot/blogmania/BlogmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.springboot.blogmania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
