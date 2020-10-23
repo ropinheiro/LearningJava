af67513d161c3cd2a926cc000bbe63a169ad5352
Rui Pinheiro <rpinheiro@neoception.com>
2020-10-23 16:28:43 +0100

    Hello Java World | Original code from a Spring Initializr generation

---
+++ hello-java-world/src/main/java/com/neoception/hellojavaworld/HelloJavaWorldApplication.java
@@ -0,0 +1,13 @@
+package com.neoception.hellojavaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJavaWorldApplication.class, args);
+	}
+
+}
