fe8c6f49ad68cb00ded9233fa8fad5cc481725ae
Rui Pinheiro <rpinheiro@neoception.com>
2020-10-30 17:46:03 +0000

    Kafka | Add dependency and imports

--- hello-java-world/src/main/java/com/neoception/hellojavaworld/HelloJavaWorldApplication.java
+++ hello-java-world/src/main/java/com/neoception/hellojavaworld/HelloJavaWorldApplication.java
@@ -3,6 +3,12 @@ package com.neoception.hellojavaworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
 @SpringBootApplication
 public class HelloJavaWorldApplication {
 
