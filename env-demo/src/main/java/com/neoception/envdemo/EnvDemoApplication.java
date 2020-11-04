994215fee6f42c6a75d7e814052dc0cc81617927
Rui Pinheiro <rpinheiro@neoception.com>
2020-11-04 08:56:32 +0000

    EnvDemo | Output JAVA_HOME and App Name to the log / console

--- env-demo/src/main/java/com/neoception/envdemo/EnvDemoApplication.java
+++ env-demo/src/main/java/com/neoception/envdemo/EnvDemoApplication.java
@@ -1,13 +1,29 @@
 package com.neoception.envdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class EnvDemoApplication {
+public class EnvDemoApplication implements CommandLineRunner {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(EnvDemoApplication.class);
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void run(String... args) {
+        logger.info("{}", "Java Home: " + env.getProperty("JAVA_HOME"));
+        logger.info("{}", "App Name: " + env.getProperty("app.name"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EnvDemoApplication.class, args);
     }
-
 }
