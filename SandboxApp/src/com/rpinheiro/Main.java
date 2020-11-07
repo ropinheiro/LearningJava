c5ba6cde31029ad44593b67a158423ad0cf5ab1f
Rui Pinheiro <rpinheiro@neoception.com>
2020-11-07 17:48:23 +0000

    SandBox | Add base console app for sandbox stuff

---
+++ SandboxApp/src/com/rpinheiro/Main.java
@@ -0,0 +1,10 @@
+package com.rpinheiro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+    }
+}
